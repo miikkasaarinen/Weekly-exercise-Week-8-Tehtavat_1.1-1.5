1a59c62fe62c56ffb6545770a4f38775728ffadd
Miikka Saarinen <miikka.saarinen@awacademy.training>
2022-08-02 10:03:23 +0300

    Tehtävät 1.4 ja 1.5 tehty.

--- src/App.js
+++ src/App.js
@@ -16,15 +16,15 @@ const Part = (props) => {
 }
 
 const Content = (props) => {
-  console.log(props)
     return(
       <div>
-        <Part part={props.part1.name} exercises={props.part1.exercises} />
-        <Part part={props.part2.name} exercises={props.part2.exercises} />
-        <Part part={props.part3.name} exercises={props.part3.exercises} />
+        <Part part={props.parts1.name} exercises={props.parts1.exercises} />
+        <Part part={props.parts2.name} exercises={props.parts2.exercises} />
+        <Part part={props.parts3.name} exercises={props.parts3.exercises} />
       </div>
     )
 }
+
 const Total = (prop) => {
   return(
     <div>
@@ -32,25 +32,30 @@ const Total = (prop) => {
     </div>
   )
 }
+
 const App = (props) => {
-  const course = 'Half Stack application development'
-  const part1 = {
+  const course = {
+  name: 'Half Stack application development',
+  parts: [ 
+  {
     name: 'Fundamentals of React',
     exercises: 10
-  }
-  const part2 = {
+  },
+  {
     name: 'Using props to pass data',
     exercises: 7
-  }
-  const part3 = {
+  },
+  {
     name: 'State of a component',
     exercises: 14
   }
+]
+}
   return (
     <div>
-      <Header header={course} />
-      <Content part1={part1} exercises1={part1} part2={part2} exercises2={part2} part3={part3} exercises3={part3}/>
-      <Total num={part1.exercises + part2.exercises + part3.exercises} />
+      <Header header={course.name} />
+      <Content parts1={course.parts[0]} parts2={course.parts[1]} parts3={course.parts[2]} />
+      <Total num={course.parts[0].exercises + course.parts[1].exercises + course.parts[2].exercises} />
     </div>
   )
 }
